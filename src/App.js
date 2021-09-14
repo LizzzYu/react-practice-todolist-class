745c45e9c0c3dea5b7eb2a131498be368338eab6
liz <qqqq929@hotmail.com>
2021-09-14 16:42:38 +0800

    finish APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Title from './components/Title';
+import Form from './components/Form';
+import List from './components/List';
 import './App.css';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let list = [
+      {
+        id: 0,
+        text: '吃早餐'
+      },
+      {
+        id: 1,
+        text: '到公司'
+      },
+      {
+        id: 2,
+        text: '吃午餐'
+      }
+    ];
+    
+    this.state = {
+      todos: list,
+      startId: 3,
+    }
+  }
+
+  createTodo(text) {
+    this.setState({
+      todos: [...this.state.todos, {
+        id: this.state.startId,
+        text: text
+      }],
+      startId: this.state.startId + 1
+    })
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id),
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Title todos={this.state.todos} />
+        <Form createTodo={text => this.createTodo(text)} />
+        <List
+          todos={this.state.todos}
+          removeTodo={id => this.removeTodo(id)} />
+      </div>
+    );
+  }
 }
 
 export default App;
